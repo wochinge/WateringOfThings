88437ed453062518cc99bf0ba7f5ea5b85a529b4
Tobias Wochinger <tobias.wochinger@student.htw-berlin.de>
2016-10-21 14:41:22 +0200

    test view to retrieve plants

--- server/api/app/views.py
+++ server/api/app/views.py
@@ -1,22 +1,30 @@
-from django.shortcuts import render
-
 # Create your views here.
-from django.contrib.auth.models import User, Group
-from rest_framework import viewsets
-from api.app.serializers import UserSerializer, GroupSerializer
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from api.app.models import Plant, MoistureValue
+from api.app.serializers import PlantSerializer, MoistureValueSerializer
 
 
-class UserViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
-    queryset = User.objects.all().order_by('-date_joined')
-    serializer_class = UserSerializer
+class PlantView(APIView):
+
+    def get(self, request, controllerID, format=None):
+        plants = Plant.objects.all()
+        serializer = PlantSerializer(plants, many=True)
+        return Response(serializer.data)
 
 
-class GroupViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows groups to be viewed or edited.
-    """
-    queryset = Group.objects.all()
-    serializer_class = GroupSerializer
+
+# class UserViewSet(viewsets.ModelViewSet):
+#     """
+#     API endpoint that allows users to be viewed or edited.
+#     """
+#     queryset = User.objects.all().order_by('-date_joined')
+#     serializer_class = UserSerializer
+
+
+# class GroupViewSet(viewsets.ModelViewSet):
+#     """
+#     API endpoint that allows groups to be viewed or edited.
+#     """
+#     queryset = Group.objects.all()
+#     serializer_class = GroupSerializer
--- server/api/urls.py
+++ server/api/urls.py
@@ -13,20 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls import url, include
 from rest_framework import routers
 from api.app import views
 
 router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
+
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include(router.urls)),
+    url(r'^microController/(?P<controllerID>.+)/plant/$', views.PlantView.as_view()),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
