2121f4310116059319b72452398c4341ffcf4efa
Tobias Wochinger <tobias.wochinger@student.htw-berlin.de>
2016-10-24 14:00:15 +0200

    implemented a function to water plants

--- server/api/app/mqtt.py
+++ server/api/app/mqtt.py
@@ -1,28 +1,31 @@
 from __future__ import absolute_import
 import paho.mqtt.client as mqtt
-from api.app.models import MicroController
+from api.app.models import MicroController, Plant, MoistureValue
 import logging
+import json
 
 INDEX_CONTROLLER_ID = 2
 INDEX_ACTUAL_TOPIC = 3
 INDEX_PIN = 4
 logger = logging.getLogger(__name__)
 
+MATCH_ANY = '+'
+BASE_TOPIC = 'WateringOfPlants/microController/'
+MOISTURE_VALUES_TOPIC = BASE_TOPIC + MATCH_ANY + '/measuredValues/' + MATCH_ANY
+
 
 def water_topic(micro_controller_id):
-    return 'WateringOfPlants/microController/' + micro_controller_id + '/water'
+    return BASE_TOPIC + micro_controller_id + '/water'
 
 
 def on_connect(client, userdata, flags, rc):
     logger.info('Mqtt client connected')
-    client.subscribe('WateringOfPlants/microController/+/measuredValues/+')
+    client.subscribe('WateringOfPlants/microController/' + MATCH_ANY + '/measuredValues/' + MATCH_ANY)
 
 
-def on_message(client, userdata, msg):
+def on_moisture_values_measured(client, userdata, msg):
     split = msg.topic.split('/')
-
-    if len(split) < 5:
-        return
+    logger.error(msg.topic)
 
     micro_controller_id = split[INDEX_CONTROLLER_ID]
     micro_controller_exists = MicroController.objects.filter(pk=micro_controller_id).count() == 1
@@ -33,16 +36,20 @@ def on_message(client, userdata, msg):
     if split[INDEX_ACTUAL_TOPIC] == 'measuredValues':
         pin = split[INDEX_PIN]
         value = msg.payload
+        plant = Plant.objects.get(microController_id=micro_controller_id, pin=pin)
+        MoistureValue.objects.create(plant=plant, value=value)
 
 
-def water_plant(micro_controller_id, pin, time):
+def water_plant(micro_controller_id, plant, time):
     topic = water_topic(micro_controller_id)
     logger.error(topic)
-    client.publish(topic, time)
+    message = {'position': plant.position, 'time': time}
+    client.publish(topic, json.dumps(message))
+
 
 client = mqtt.Client()
 client.on_connect = on_connect
-client.on_message = on_message
-client.connect_async('test.mosca.io', 1883, 60)
+client.message_callback_add(MOISTURE_VALUES_TOPIC, on_moisture_values_measured)
+client.connect_async(host='test.mosca.io', port=1883, keepalive=60)
 client.loop_start()
 
