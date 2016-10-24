f454a2163223edae1d7e20e7839985e2a0d77404
Tobias Wochinger <tobias.wochinger@student.htw-berlin.de>
2016-10-24 14:00:32 +0200

    delete, get plant and get moisture values

--- server/api/app/views.py
+++ server/api/app/views.py
@@ -1,8 +1,8 @@
-# Create your views here.
 from rest_framework import status
+from django.core.exceptions import ObjectDoesNotExist
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from api.app.models import MicroController, Plant, MoistureValue
+from api.app.models import MicroController, Plant
 from api.app.serializers import MicroControllerSerializer, PlantSerializer, MoistureValueSerializer
 from api.app.mqtt import water_plant
 
@@ -11,8 +11,15 @@ def exists_microcontroller_id(controller_id):
     return MicroController.objects.filter(id=controller_id).count() == 1
 
 
+def get_plant(controller_id, plant_id):
+    try:
+        return Plant.objects.get(id=plant_id, microController__id=controller_id)
+    except ObjectDoesNotExist:
+        return None
+
+
 def exists_plant(controller_id, plant_id):
-    return Plant.objects.filter(id=plant_id, microController__id=controller_id).count() == 1
+    return get_plant(controller_id, plant_id) is not None
 
 
 class MicroControllerView(APIView):
@@ -24,36 +31,64 @@ class MicroControllerView(APIView):
         return Response(status=status.HTTP_201_CREATED)
 
 
-class PlantView(APIView):
+class AllPlantsView(APIView):
 
-    def get(self, request, controller_id, format=None):
+    def get(self, request, controller_id):
         if not exists_microcontroller_id(controller_id):
-            return Response(status=status.HTTP_401_UNAUTHORIZED)
+            return Response(status=status.HTTP_400_BAD_REQUEST)
 
         plants = Plant.objects.filter(microController__id=controller_id)
         serializer = PlantSerializer(plants, many=True)
-        water_plant('1234', 100)
         return Response(serializer.data)
 
     def post(self, request, controller_id):
         if not exists_microcontroller_id(controller_id):
-            return Response(status=status.HTTP_401_UNAUTHORIZED)
+            return Response(status=status.HTTP_400_BAD_REQUEST)
 
         serializer = PlantSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save(microController_id=controller_id)
 
 
+class PlantView(APIView):
+
+    def get(self, request, controller_id, plant_id):
+        plant = get_plant(controller_id, plant_id)
+        if plant is None:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response(PlantSerializer(plant).data)
+
+    def delete(self, request, controller_id, plant_id):
+        plant = get_plant(controller_id, plant_id)
+        if plant is None:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        else:
+            plant.delete()
+
+
 class WaterPlantView(APIView):
 
-    def post(self, request, controller_id, plant_id, amount, format=None):
-        if not exists_plant(controller_id, plant_id):
-            return Response(status=status.HTTP_401_UNAUTHORIZED)
+    def post(self, request, controller_id, plant_id, amount):
+        plant = get_plant(controller_id, plant_id)
 
-        plant = Plant.objects.get(id=plant_id)
-        water_plant(controller_id, plant.pin, amount)
+        if plant is None:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+        water_plant(controller_id, plant, amount)
         return Response(status=status.HTTP_200_OK)
 
+
+class MoistureView(APIView):
+
+    def get(self, request, controller_id, plant_id):
+        plant = get_plant(controller_id, plant_id)
+
+        if plant is None:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+        moisture_values = plant.moisturevalue_set.all()
+        return Response(MoistureValueSerializer(moisture_values, many=True).data)
 
 
 
--- server/api/urls.py
+++ server/api/urls.py
@@ -27,7 +27,11 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include(router.urls)),
     url(r'^microController/$', views.MicroControllerView.as_view()),
-    url(r'^microController/(?P<controller_id>.+)/plant/$', views.PlantView.as_view()),
-    url(r'^microController/(?P<controller_id>.+)/plant/(?P<plant_id>.+)/water/(?P<amount>.+)/$', views.WaterPlantView.as_view()),
+    url(r'^microController/(?P<controller_id>.+)/plant/(?P<plant_id>.+)/water/(?P<amount>.+)/$',
+        views.WaterPlantView.as_view()),
+    url(r'^microController/(?P<controller_id>.+)/plant/(?P<plant_id>.+)/moisture/$',
+        views.MoistureView.as_view()),
+    url(r'^microController/(?P<controller_id>.+)/plant/(?P<plant_id>.+)/$', views.PlantView.as_view()),
+    url(r'^microController/(?P<controller_id>.+)/plant/$', views.AllPlantsView.as_view()),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
