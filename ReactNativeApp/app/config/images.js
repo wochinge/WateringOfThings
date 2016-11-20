1493454f6d2272a11280e95777928ff8fb2c06ba
Tobias Wochinger <tobias.wochinger@student.htw-berlin.de>
2016-11-20 22:27:08 +0100

    added icons

--- ReactNativeApp/app/config/images.js
+++ ReactNativeApp/app/config/images.js
@@ -1,5 +1,8 @@
 const images = {
   basil: require('../images/Basil.jpg'),
+  cactus: require('../images/cactus.png'),
+  lotus: require('../images/lotus.png'),
+  defaultPlantImage: require('../images/default_plant.png')
 };
 
 export { images };
