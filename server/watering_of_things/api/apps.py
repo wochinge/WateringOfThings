713431eadb817dbc6461a826a5d8f3a10624eff3
Tobias Wochinger <tobias.wochinger@student.htw-berlin.de>
2017-01-04 17:52:06 +0100

    removed initial db stuff

--- server/watering_of_things/api/apps.py
+++ server/watering_of_things/api/apps.py
@@ -3,18 +3,3 @@ from django.apps import AppConfig
 
 class AppConfig(AppConfig):
     name = 'watering_of_things.api'
-
-    def ready(self):
-        from watering_of_things.api.models import MicroController, Plant, MoistureValue
-        controller, _ = MicroController.objects.get_or_create(id='3')
-        controller, _ = MicroController.objects.get_or_create(id='123456789')
-        raspberry, _ = Plant.objects.get_or_create(name="Himbeere", pin=1, position=0, moistureThreshold=22.0, microController=controller)
-        MoistureValue.objects.get_or_create(plant=raspberry, value=50)
-        MoistureValue.objects.get_or_create(plant=raspberry, value=100)
-
-        cactus, _ = Plant.objects.get_or_create(name="Kaktus", pin=2, position=45, moistureThreshold=5.0, microController=controller)
-        MoistureValue.objects.get_or_create(plant=cactus, value=10)
-
-        rose, _ = Plant.objects.get_or_create(name="Rose", pin=4, position=100, moistureThreshold=50.0, microController=controller)
-        MoistureValue.objects.get_or_create(plant=rose, value=0)
-
