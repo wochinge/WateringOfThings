10f99ad2b38c0f291af064e85ca5a32342dd969e
Tobias Wochinger <tobias.wochinger@student.htw-berlin.de>
2017-01-04 20:45:47 +0100

    removed example config

--- server/watering_of_things/config/mqtt_settings.py
+++ server/watering_of_things/config/mqtt_settings.py
@@ -1,4 +1,4 @@
-SERVER = "mqtt.example.com"
-PORT = 1234
-USERNAME = 'username'
-PASSWORD = "pass"
+SERVER = "mqtt.wocit.ankaa.uberspace.de"
+PORT = 64517
+USERNAME = 'watering_of_things_server'
+PASSWORD = "UdayM'7#1V5OTfh%1NsH4;F7,[pi'3"
