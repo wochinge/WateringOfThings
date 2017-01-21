0cabc125e32fbd603f599062204a18599a590437
Doreen Sacker <dsacker@gmx.de>
2017-01-21 20:34:29 +0100

    water view

--- ReactNativeApp/app/routes/WaterPlant/waterPlant.js
+++ ReactNativeApp/app/routes/WaterPlant/waterPlant.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { View, Text, StyleSheet, Image, Slider, ScrollView } from 'react-native';
 import { images, commonStyles, I18n} from '../../config';
 import Button from 'apsl-react-native-button';
-import { PlantStatus } from '../../components';
 import { connect } from 'react-redux';
 import autobind from 'autobind-decorator';
 
@@ -20,28 +19,70 @@ class WaterPlantView extends Component {
     this.state = {
       amount: 50,
     };
+    this.state = {
+      imageURL: images.glass1
+    };
   }
 
   render() {
     return (
       <View style={styles.container}>
         <ScrollView>
+          <Image style={styles.image} source={this.state.imageURL}/>
+
           <View style={styles.innerContainer}>
-            <PlantStatus plant={this.props.plant} style={styles.moistureText}/>
               <View
                 style={styles.sliderImages}>
-                <Image
-                  source={images.cactus}
-                  style={styles.sliderIcon}/>
+                <Text>
+                  less
+                </Text>
                 <Text>
                   {this.state.amount} ml
                 </Text>
-                <Image
-                  style={styles.sliderIcon}
-                  source={images.lotus}/>
+                <Text>
+                  more
+                </Text>
               </View>
               <Slider style={styles.slider}
-                onValueChange={(amount) => this.setState({amount})}
+                onValueChange={(amount) => {
+                  let tempImage;
+                  if(amount > 180){
+                    tempImage = images.glass10;
+                  }
+                  else if(amount > 160){
+                    tempImage = images.glass9;
+                  }
+                  else if(amount > 140){
+                    tempImage = images.glass8;
+                  }
+                  else if(amount > 120){
+                    tempImage = images.glass7;
+                  }
+                  else if(amount > 100){
+                    tempImage = images.glass6;
+                  }
+                  else if(amount > 80){
+                    tempImage = images.glass5;
+                  }
+                  else if(amount > 60){
+                    tempImage = images.glass4;
+                  }
+                  else if(amount > 40){
+                    tempImage = images.glass3;
+                  }
+                  else if(amount > 20){
+                    tempImage = images.glass2;
+                  }
+                  else if(amount < 20){
+                    tempImage = images.glass1;
+                  }
+                  return(this.setState({
+                    amount: amount,
+                    imageURL: tempImage
+                  })
+                  );
+                }
+              }
                 minimumValue={1.0}
                 maximumValue={200.0}
                 step={1}
@@ -83,22 +124,19 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
+  image:{
+    height: 167,
+    width: 105,
+    alignSelf: 'center',
+  },
   innerContainer: {
     borderRadius: 10,
     margin: 60,
     flex:1,
   },
-  moistureText:{
-    textAlign: 'left',
-    padding: 20,
-  },
   sliderImages: {
     flexDirection: 'row',
     justifyContent: 'space-between'
-  },
-  sliderIcon: {
-    width: 50,
-    height: 50
   },
   slider: {
     width: 250,
