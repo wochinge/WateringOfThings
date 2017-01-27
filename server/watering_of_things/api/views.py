33ad5e1eb55c2253938bb577f9903c1845c0beed
Tobias Wochinger <tobias.wochinger@student.htw-berlin.de>
2017-01-27 16:01:51 +0100

    refactoring with django shortcuts

--- server/watering_of_things/api/views.py
+++ server/watering_of_things/api/views.py
@@ -2,6 +2,7 @@ from rest_framework import status
 from django.core.exceptions import ObjectDoesNotExist
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from django.shortcuts import get_object_or_404
 from watering_of_things.api.models import MicroController, Plant
 from watering_of_things.api.serializers import PlantSerializer, MoistureValueSerializer
 from watering_of_things.api.mqtt import water_plant, request_moisture_values
@@ -11,15 +12,12 @@ def exists_microcontroller_id(controller_id):
     return MicroController.objects.filter(id=controller_id).count() == 1
 
 
+def validate_controller(controller_id):
+    get_object_or_404(MicroController, id=controller_id)
+
+
 def get_plant(controller_id, plant_id):
-    try:
-        return Plant.objects.get(id=plant_id, microController__id=controller_id)
-    except ObjectDoesNotExist:
-        return None
-
-
-def exists_plant(controller_id, plant_id):
-    return get_plant(controller_id, plant_id) is not None
+    return get_object_or_404(Plant, id=plant_id, microController__id=controller_id)
 
 
 class ControllerView(APIView):
@@ -35,8 +33,7 @@ class ControllerView(APIView):
 class AllPlantsView(APIView):
 
     def get(self, request, controller_id):
-        if not exists_microcontroller_id(controller_id):
-            return Response(status=status.HTTP_400_BAD_REQUEST)
+        validate_controller(controller_id)
 
         plants = Plant.objects.filter(microController__id=controller_id)
         serializer = PlantSerializer(plants, many=True)
@@ -57,38 +54,27 @@ class PlantView(APIView):
 
     def get(self, request, controller_id, plant_id):
         plant = get_plant(controller_id, plant_id)
-        if plant is None:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
-        else:
-            return Response(PlantSerializer(plant).data)
+        return Response(PlantSerializer(plant).data)
 
     def put(self, request, controller_id, plant_id):
         plant = get_plant(controller_id, plant_id)
         updated = PlantSerializer(data=request.data)
-        if plant is None or not updated.is_valid():
-            return Response(status=status.HTTP_400_BAD_REQUEST)
-        else:
+        if updated.is_valid():
             updated.update(plant, updated.validated_data)
             return Response(status=status.HTTP_200_OK, data=updated.validated_data)
-
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, controller_id, plant_id):
         plant = get_plant(controller_id, plant_id)
-        if plant is None:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
-        else:
-            plant.delete()
-            return Response(status=status.HTTP_200_OK)
+        plant.delete()
+        return Response(status=status.HTTP_200_OK)
 
 
 class WaterPlantView(APIView):
 
     def post(self, request, controller_id, plant_id, amount):
         plant = get_plant(controller_id, plant_id)
-
-        if plant is None:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
-
         water_plant(controller_id, plant, amount)
         return Response(status=status.HTTP_200_OK)
 
@@ -97,12 +83,5 @@ class MoistureView(APIView):
 
     def get(self, request, controller_id, plant_id):
         plant = get_plant(controller_id, plant_id)
-
-        if plant is None:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
-
         moisture_values = plant.moisturevalue_set.all()
         return Response(MoistureValueSerializer(moisture_values, many=True).data)
-
-
-
